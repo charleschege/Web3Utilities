fb8d2e0acf257070146bbcd7667958be32d85392
Charles Chege <charleschege@protonmail.ch>
2022-07-11 09:47:26 +0300

    Feat: Add new type  commonly used in web3

---
+++ src/data_types.rs
@@ -0,0 +1,12 @@
+pub type Ed25519PublicKeyBytes = [u8; 32];
+pub type Ed25519SecretKeyBytes = [u8; 32];
+pub type Ed25519SignatureBytes = [u8; 64];
+pub type Sr25519PublicKeyBytes = [u8; 32];
+pub type Sr25519SecretKeyBytes = [u8; 32];
+pub type Sr25519SignatureBytes = [u8; 64];
+pub type X25519PublicKeyBytes = [u8; 32];
+pub type X25519SecretKeyBytes = [u8; 32];
+pub type RandomID = [u8; 32];
+pub type MessageID = [u8; 32];
+pub type TaiTimestamp = [u8; 32];
+pub type TimestampSeconds = i64;
